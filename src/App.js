0fa0833dbe18b0ed6ecfc9d1d2f504ff37ec1fea
Cesar Herrera <cesarherr321@gmail.com>
2023-07-01 21:55:05 -0400

    Create board and turns assignments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const turns = {
+  X: "x",
+  O: "o",
+};
+
+const Square = ({ children, updateBoard, isSelected, index }) => {
+  const className = `square ${isSelected ? `is-selected` : ""}`;
+
+  const handleClick = () => {
+    updateBoard()
+  }
+
+  return <div onClick={handleClick} className={className}>{children}</div>;
+};
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(""));
+
+  const [turn, setTurn] = useState(turns.X);
+
+  const updateBoard = () => {
+    const newTurn = turn === turns.X ? turns.O : turns.X;
+    setTurn(newTurn)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <section className='game'>
+        {board.map((_, index) => {
+          return (
+          <Square 
+          key={index} 
+          index={index}
+          updateBoard={updateBoard}>
+
+          </Square>
+          );
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === turns.X}>{turns.X}</Square>
+        <Square isSelected={turn === turns.O}>{turns.O}</Square>
+      </section>
+    </main>
   );
 }
 
