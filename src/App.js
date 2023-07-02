127f876f04e6a94656df795d5e6e8372544c3329
Cesar Herrera <cesarherr321@gmail.com>
2023-07-01 22:54:06 -0400

    Add winner and stop the game

--- src/App.js
+++ src/App.js
@@ -10,21 +10,66 @@ const Square = ({ children, updateBoard, isSelected, index }) => {
   const className = `square ${isSelected ? `is-selected` : ""}`;
 
   const handleClick = () => {
-    updateBoard()
-  }
+    updateBoard(index);
+  };
 
-  return <div onClick={handleClick} className={className}>{children}</div>;
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
 };
 
+const winnerCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(""));
+  const [board, setBoard] = useState(Array(9).fill(null));
 
   const [turn, setTurn] = useState(turns.X);
 
-  const updateBoard = () => {
-    const newTurn = turn === turns.X ? turns.O : turns.X;
-    setTurn(newTurn)
+  const [winner, setWinner] = useState(null);  
+
+  const checkWinner = (boardCheck) => {
+    for (let i of winnerCombos) {
+      const [a,b,c] = i;
+      console.log(a)
+      if(
+        boardCheck[a] && boardCheck[a] === boardCheck[b] && boardCheck[a] === boardCheck[c]
+      ){
+        
+        return boardCheck[a]
+      }
+    }
+    return null
   }
+
+  const updateBoard = (index) => {
+
+    if (board[index] || winner) return;
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    
+    setBoard(newBoard);
+
+    const newTurn = turn === turns.X ? turns.O : turns.X;
+    setTurn(newTurn);
+
+    const newWinner = checkWinner(newBoard)
+
+    if(newWinner){
+      setWinner(newWinner)
+    }
+  };
 
   return (
     <main className='board'>
@@ -32,16 +77,13 @@ function App() {
       <section className='game'>
         {board.map((_, index) => {
           return (
-          <Square 
-          key={index} 
-          index={index}
-          updateBoard={updateBoard}>
-
-          </Square>
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
           );
         })}
       </section>
-      <section className="turn">
+      <section className='turn'>
         <Square isSelected={turn === turns.X}>{turns.X}</Square>
         <Square isSelected={turn === turns.O}>{turns.O}</Square>
       </section>
